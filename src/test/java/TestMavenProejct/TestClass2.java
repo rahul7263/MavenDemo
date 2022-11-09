fc1599aa300dbac26906df404627bc516dc9f6ef
Rahul <Rahul@DESKTOP-OVJ75DL>
2022-11-09 23:37:58 +0530

    Maven Demo

---
+++ src/test/java/TestMavenProejct/TestClass2.java
@@ -0,0 +1,21 @@
+package TestMavenProejct;
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+   
+	@Test
+	public void testcase14() {
+		System.out.println("Maven Project Demo ");
+	}
+	
+	@Test
+    public void testcase5() {
+    	System.out.println("Maven Project Demo ");
+	}
+    
+	@Test
+    public void testcase6() {
+    	System.out.println("Maven Project Demo ");
+	}
+}
